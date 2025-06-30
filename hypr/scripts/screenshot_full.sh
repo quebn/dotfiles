8ea5bc50b73e3b9fa19f1b7c53d21a5e65baa788
Quebn <gonzalesquebin@gmail.com>
2025-06-30 23:26:17 +0800

    hypr: update script and windowrules

--- hypr/scripts/screenshot_full.sh
+++ hypr/scripts/screenshot_full.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 
-dir=$HOME/Pictures/screenshots/
-filefmt=$dir/$(date +%s)_grim.png
+dir="$HOME/Pictures/screenshots/"
+filename="grim-$(date +%s).png"
+filepath="$dir/$filename"
 
-grim "$filefmt" && notify-send "Screenshot saved" -i "$filefmt"
+grim "$filepath" && notify-send \
+    "Screenshot saved" \
+    "~/Pictures/screenshots/$filename" \
+    -i "$filepath" \
+    -a "Grim"\
+    --action=default,"Open Image"
+
+ACTION=$(dunstctl action)
+echo "$ACTION"
+if [[ "$ACTION" = "default" ]]; then
+    xdg-open "$filepath"
+fi
