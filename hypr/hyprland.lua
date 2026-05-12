e6cfdc7753ac898d4ab69e74e1beb18af5f7c618
Quebn <gonzalesquebin@gmail.com>
2026-05-12 22:35:35 +0800

    hypr: remove theme.conf and fix default wall and splash text to be properly hidden

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,3 +1,28 @@
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "1.2",
+})
+
+local app_runner  = "runapp"
+local terminal    = app_runner.." kitty"
+local browser     = app_runner.." zen-browser"
+local google      = app_runner.." chromium"
+local menu        = "rofi -show drun -run-command \""..app_runner.." {cmd}\""
+local colorpicker = "hyprpicker -a"
+local webapp      = google .. " --new-window --app="
+
+-- hl.on("hyprland.start", function()
+--   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+-- end)
+
+hl.on("hyprland.start", function()
+    hl.exec_cmd(terminal)
+    hl.exec_cmd(browser)
+    hl.exec_cmd("systemctl --user start plasma-polkit-agent")
+end)
+
 local colors = {
     rose_pine = {
         night           = "rgba(000000ff)",
@@ -19,38 +44,10 @@ local colors = {
         highlight_low   = "rgba(21202eff)",
         highlight_med   = "rgba(403d52ff)",
         highlight_high  = "rgba(110d12ff)",
+        highlight_high_alt = "rgba(6e6a85ff)",
     }
 }
 
-local app_runner  = "app2unit"
-local terminal    = app_runner.." kitty"
-local browser     = app_runner.." zen-browser"
-local google      = app_runner.." chromium"
-local menu        = "rofi -show drun -run-command \""..app_runner.." {cmd}\""
-local colorpicker = "hyprpicker -a"
-local webapp      = google .. " --new-window --app"
-
-local screenshot_edit = "$HOME/.config/hypr/scripts/screenshot_edit.sh"
-local screenshot_full = "$HOME/.config/hypr/scripts/screenshot_full.sh"
-local powermenu       = "$HOME/.config/hypr/scripts/powermenu.sh"
-
-hl.monitor({
-    output   = "eDP-1",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "1.2",
-})
-
-hl.on("hyprland.start", function()
-    -- hl.exec_cmd("hyprpaper")
-    hl.exec_cmd(terminal, {
-        workspace = "1 silent",
-    })
-    hl.exec_cmd(browser, {
-        workspace = "2 silent",
-    })
-    hl.exec_cmd("systemctl --user start plasma-polkit-agent")
-end)
 
 hl.config({
     xwayland = {
@@ -104,12 +101,13 @@ hl.config({
         fullscreen_on_one_column = true,
     },
     misc = {
-        disable_splash_rendering = false,
+        disable_splash_rendering = true,
+        disable_hyprland_logo   = true,
+        force_default_wallpaper = 0,
         splash_font_family = "ZxProto, Sans",
         font_family = "ZxProto, Sans",
-        force_default_wallpaper = 0,
-        disable_hyprland_logo   = false,
-        focus_on_activate = false
+        focus_on_activate = false,
+        vrr = 1,
     },
     input = {
         kb_layout  = "us",
@@ -123,19 +121,24 @@ hl.config({
     animations = {
         enabled = true,
     },
+    ecosystem = {
+        no_update_news = true,
+        no_donation_nag = true,
+    },
+    quirks = {
+        prefer_hdr = true,
+    }
 })
 
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 
-hl.curve("myBezier",       { type = "bezier", points = { {0.15, 0.9},  {0.1, 1}    } })
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1  },  {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
 hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
 hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 
--- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+-- hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 5,    bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 2.39, bezier = "linear" })
@@ -150,40 +153,43 @@ hl.animation({ leaf = "layersIn",      enabled = true,  speed = 3,    bezier = "
 hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
 hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1,    bezier = "myBezier", style = "slide" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1,    bezier = "quick", style = "slide" })
 hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 2,    bezier = "quick" })
 -- hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
 -- hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
 
 
-hl.device({
-    name        = "epic-mouse-v1",
-    sensitivity = -0.5,
-})
+-- hl.device({
+--     name        = "epic-mouse-v1",
+--     sensitivity = -0.5,
+-- })
+
+local screenshot_edit = "$HOME/.config/hypr/scripts/screenshot_edit.sh"
+local screenshot_full = "$HOME/.config/hypr/scripts/screenshot_full.sh"
+local powermenu       = "$HOME/.config/hypr/scripts/powermenu.sh"
 
 local mod = "SUPER"
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mod .. " + B", hl.dsp.exec_cmd(browser))
-hl.bind(mod .. " + return", hl.dsp.exec_cmd(terminal))
-hl.bind(mod .. " + space", hl.dsp.exec_cmd(menu))
-hl.bind(mod .. " + SHIFT + Q", hl.dsp.exec_cmd(powermenu))
-hl.bind(mod .. " + SHIFT + X", hl.dsp.window.close())
-hl.bind(mod .. " + SHIFT + F", hl.dsp.window.fullscreen())
-hl.bind(mod .. " + F", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mod .. " + E", hl.dsp.window.move({ workspace = "empty" }))
-hl.bind(mod .. " + I", hl.dsp.exec_cmd(colorpicker))
-hl.bind(mod .. " + A", hl.dsp.exec_cmd(webapp.." https://chatgpt.com"))
-hl.bind(mod .. " + D", hl.dsp.exec_cmd(webapp.." https://excalidraw.com"))
-hl.bind(mod .. " + O", hl.dsp.workspace.toggle_special())
-hl.bind(mod .. " + semicolon", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mod.." + B", hl.dsp.exec_cmd(browser))
+hl.bind(mod.." + return", hl.dsp.exec_cmd(terminal))
+hl.bind(mod.." + space", hl.dsp.exec_cmd(menu))
+hl.bind(mod.." + SHIFT + Q", hl.dsp.exec_cmd(powermenu))
+hl.bind(mod.." + SHIFT + X", hl.dsp.window.close())
+hl.bind(mod.." + SHIFT + F", hl.dsp.window.fullscreen())
+hl.bind(mod.." + F", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mod.." + E", hl.dsp.window.move({ workspace = "empty" }))
+hl.bind(mod.." + I", hl.dsp.exec_cmd(colorpicker))
+hl.bind(mod.." + A", hl.dsp.exec_cmd(webapp.."https://chatgpt.com"))
+hl.bind(mod.." + D", hl.dsp.exec_cmd(webapp.."https://excalidraw.com"))
+hl.bind(mod.." + O", hl.dsp.workspace.toggle_special())
+hl.bind(mod.." + semicolon", hl.dsp.layout("togglesplit"))    -- dwindle only
 
 hl.bind("F11", hl.dsp.window.fullscreen())    -- dwindle only
-hl.bind(mod .. " + SHIFT + P", hl.dsp.exec_cmd("hyprctl dispatch focuswindow \"title:^(Picture-in-Picture)$\""))
-
-hl.bind(mod .. " + h", hl.dsp.focus({ direction = "left" }))
-hl.bind(mod .. " + l", hl.dsp.focus({ direction = "right" }))
-hl.bind(mod .. " + k", hl.dsp.focus({ direction = "up" }))
-hl.bind(mod .. " + j", hl.dsp.focus({ direction = "down" }))
+hl.bind(mod.. " + SHIFT + P", hl.dsp.focus({ window = "title:^(Picture-in-Picture)", }))
+hl.bind(mod.." + h", hl.dsp.focus({ direction = "left" }))
+hl.bind(mod.." + l", hl.dsp.focus({ direction = "right" }))
+hl.bind(mod.." + k", hl.dsp.focus({ direction = "up" }))
+hl.bind(mod.." + j", hl.dsp.focus({ direction = "down" }))
 
 
 for i = 1, 10 do
@@ -193,23 +199,23 @@ for i = 1, 10 do
 end
 
 -- Example special workspace (scratchpad)
-hl.bind(mod .. " + S", hl.dsp.exec_cmd(screenshot_full))
-hl.bind("Print", hl.dsp.exec_cmd(screenshot_full))
-hl.bind(mod .. " + SHIFT + S", hl.dsp.exec_cmd(screenshot_edit))
-hl.bind(mod .. " + Print", hl.dsp.exec_cmd(screenshot_edit))
+hl.bind(mod.." + S", hl.dsp.exec_cmd(screenshot_edit))
+hl.bind("Print", hl.dsp.exec_cmd(screenshot_edit))
+hl.bind(mod.." + SHIFT + S", hl.dsp.exec_cmd(screenshot_full))
+hl.bind(mod.." + Print", hl.dsp.exec_cmd(screenshot_full))
 
 -- Scroll through existing workspaces with mainMod + scroll
-hl.bind(mod .. " + bracketright", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(mod .. " + bracketleft",  hl.dsp.focus({ workspace = "e-1" }))
-hl.bind(mod .. " + SHIFT + bracketright", hl.dsp.window.move({ workspace = "e+1" }))
-hl.bind(mod .. " + SHIFT + bracketleft",  hl.dsp.window.move({ workspace = "e-1" }))
-hl.bind(mod .. " + mouse_down",   hl.dsp.exec_cmd("hyprctl -q keyword cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '(.float * 1.5) | if . > 5 then 5 else . end')"))
-hl.bind(mod .. " + mouse_up", hl.dsp.exec_cmd("hyprctl -q keyword cursor:zoom_factor 1"))
-hl.bind(mod .. " + escape",   hl.dsp.exec_cmd("hyprctl -q keyword cursor:zoom_factor 1"))
+hl.bind(mod.." + bracketright", hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(mod.." + bracketleft",  hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(mod.." + SHIFT + bracketright", hl.dsp.window.move({ workspace = "e+1" }))
+hl.bind(mod.." + SHIFT + bracketleft",  hl.dsp.window.move({ workspace = "e-1" }))
+-- hl.bind(mod.." + mouse_down",   hl.dsp.exec_cmd("hyprctl -q eval cursor:zoom_factor $(hyprctl getoption cursor:zoom_factor -j | jq '(.float * 1.5) | if . > 5 then 5 else . end')"))
+-- hl.bind(mod.." + mouse_up", hl.dsp.exec_cmd("hyprctl -q eval cursor:zoom_factor 1"))
+-- hl.bind(mod.." + escape",   hl.dsp.exec_cmd("hyprctl -q eval cursor:zoom_factor 1"))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
-hl.bind(mod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
-hl.bind(mod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
+hl.bind(mod.." + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mod.." + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Laptop multimedia keys for volume and LCD brightness
 hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
@@ -220,23 +226,22 @@ hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+")
 hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
 
 -- Requires playerctl
-hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
-
-
---------------------------------
----- WINDOWS AND WORKSPACES ----
---------------------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
--- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
-
--- Example window rules that are useful
+-- hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+-- hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+-- hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+-- hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
 hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
+    name  = "Border Highlight on 2 or more windows",
+    match = {
+        workspace = "w[t2-10]",
+    },
+    border_color = colors.rose_pine.text
+    -- suppress_event = "maximize",
+})
+
+
+hl.window_rule({
     name  = "suppress-maximize-events",
     match = { class = ".*" },
 
@@ -244,9 +249,9 @@ hl.window_rule({
 })
 
 hl.window_rule({
-    name = "GDK Portal",
+    name = "Modal",
     match = {
-        class = "^(xdg-desktop-portal-gtk)$"
+        modal = true
     },
     float = true,
     center = true,
@@ -256,7 +261,7 @@ hl.window_rule({
 hl.window_rule({
     name = "Dialogs",
     match = {
-        class = "^(file_progress|dialogs|confirm)$"
+        class = "^(file_progress|dialogs|confirm|xdg-desktop-portal-gtk)$"
     },
     float = true,
     center = true,
@@ -408,11 +413,12 @@ hl.window_rule({
 hl.window_rule({
     name = "Picture-in-Picture",
     match = {
-        class = "^(Picture-in-Picture)$"
+        title = "^(Picture-in-Picture)$"
     },
     float = true,
     pin   = true,
     move = {"(monitor_w-window_w-20)", "(monitor_h-window_h-20)"},
+    border_color = colors.rose_pine.highlight_high_alt,
     border_size = 0,
 })
 
@@ -424,6 +430,14 @@ hl.window_rule({
     border_size = 2,
     size  = {"(window_w*1)", "(window_h*1)"},
     border_color = colors.rose_pine.surface_alt,
+})
+
+hl.window_rule({
+    name = "Zen Browser",
+    match = {
+        class = "^(zen)$",
+    },
+    workspace = "2 silent",
 })
 
 hl.window_rule({
